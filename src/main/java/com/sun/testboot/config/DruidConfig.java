3fb73b67da00f0c17f42dd5a36bd17be2450c665
sun <zheqixiaozi@163.com>
2020-05-26 19:12:23 +0800

    springboot集成druid

---
+++ src/main/java/com/sun/testboot/config/DruidConfig.java
@@ -0,0 +1,35 @@
+package com.sun.testboot.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("loginUsername","admin");
+        initParams.put("loginPassword","123456");
+        initParams.put("allow","");
+        initParams.put("deny","192.168.192.5");
+        bean.setInitParameters(initParams);
+        return bean;
+
+    }
+}
